120e29d3aedd58af9a5f1d508bb64dbc25656cb7
Ramon Castillo Luna <ramon.castillo@optimo.design>
2022-09-20 22:43:43 +0200

    relacion user estudiante migrada

---
+++ UniversityApiBackend/Migrations/20220920204238_establece-relacion-User-estudiante.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace UniversityApiBackend.Migrations
+{
+    public partial class establecerelacionUserestudiante : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "UserId",
+                table: "Estudiantes",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Estudiantes_UserId",
+                table: "Estudiantes",
+                column: "UserId",
+                unique: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Estudiantes_Users_UserId",
+                table: "Estudiantes",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Estudiantes_Users_UserId",
+                table: "Estudiantes");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Estudiantes_UserId",
+                table: "Estudiantes");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "Estudiantes");
+        }
+    }
+}
