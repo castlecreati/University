8ea1294c59655bea2a9d51c0589293f7a509fe3a
castlecreati <castlecreativa@gmail.com>
2022-11-03 19:21:57 +0100

    Quitadas referencias circulares con Fluent API y [JsonIgnore]

--- UniversityApiBackend/Models/DataModels/Chapter.cs
+++ UniversityApiBackend/Models/DataModels/Chapter.cs
@@ -1,10 +1,12 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
 
 namespace UniversityApiBackend.Models.DataModels
 {
 	public class Chapter: BaseEntity
 	{
 		public int CursoId { get; set; }
+		[JsonIgnore]
 		public virtual Curso Curso { get; set; }
 
 		[Required]
